02877850141b2d6420f1b7f773214d58f6662ff7
kumarvna <36370570+kumarvna@users.noreply.github.com>
2021-08-19 00:25:37 +0530

    updating examples and documentation

--- examples/container_registry_with_private_endpoint/main.tf
+++ examples/container_registry_with_private_endpoint/main.tf
@@ -4,9 +4,9 @@ provider "azurerm" {
 }
 
 module "container-registry" {
- // source  = "kumarvna/container-registry/azurerm"
- // version = "1.0.0"
- source = "../../"
+  source  = "kumarvna/container-registry/azurerm"
+  version = "1.0.0"
+
 
   # By default, this module will not create a resource group. Location will be same as existing RG.
   # proivde a name to use an existing resource group, specify the existing resource group name, 
